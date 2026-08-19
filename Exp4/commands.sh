8c3469decdc4f93a575456d97f3a5dd07f14058e
Kanika2007-sys <sreekanikadevik.cse2024@citchennai.net>
2026-08-19 20:56:22 +0530

    Delete Exp4 directory

--- Exp4/commands.sh
+++
@@ -1,9 +0,0 @@
-# Deploy the static site to App Engine
-gcloud app deploy
-
-# Optional flags:
-#   --project [YOUR_PROJECT_ID]   specify an alternate Cloud project ID
-#   -v [YOUR_VERSION_ID]          specify a version ID
-
-# Open the deployed app in a browser
-gcloud app browse
